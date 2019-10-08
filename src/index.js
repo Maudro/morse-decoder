bef2203720f27ae13dc38744e6bb13c7e0de41d5
maudro <maudro29@gmail.com>
2019-10-08 15:14:16 +0300

    complited

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let massiv = translateInMorse(expr);
+    let i = 0;
+    let result = "";
+    for (i; i < massiv.length;i++) {
+        if (massiv[i] != ""){
+            result += MORSE_TABLE[massiv[i]];
+        } else {
+            result += " ";
+        }
+        
+    }
+    return result;
+}
+
+function split (spr) {
+    let letters = [];
+    let counter = 0;
+    for (let i = 0; i < spr.length;i += 10) {
+        letters[counter] = spr.slice(i, i + 10);
+        counter++;
+    }
+    return letters;
+}
+
+function translateInMorse (spr) {
+    let massiv = split(spr);
+    let i = 0;
+    let result = [];
+    for (i; i < massiv.length; i++) {
+        let string = "";
+        for (let y = 0; y <= 4; y++) {            
+            if (massiv[i].slice(y * 2, y * 2 + 2) == '10') {
+                string += ".";
+            } else if (massiv[i].slice(y * 2, y * 2 + 2) == '11'){
+                string += "-";
+            }           
+        }
+        result[i] = string;
+    }
+    return result;
 }
 
 module.exports = {
